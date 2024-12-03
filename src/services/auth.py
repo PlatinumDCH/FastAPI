b14bdfd7cbdcc94ac451cd1bfb172f52fb0b1801
dima cheban <dimacheban23@gmail.com>
2024-12-03 19:37:02 +0200

    переделал импорт сессии на ассинхронный так как у нас асинхронное приложение

--- src/services/auth.py
+++ src/services/auth.py
@@ -4,6 +4,6 @@ from typing import Optional
 from fastapi import Depends, HTTPException
 from pathlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
 from starlette import status
