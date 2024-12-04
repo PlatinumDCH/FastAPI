babef350d35cee4f28e764079b13d238fec867a1
dima cheban <dimacheban23@gmail.com>
2024-12-04 12:09:55 +0200

    начал создавать класс для аутентификации пользователя

--- src/services/auth.py
+++ src/services/auth.py
@@ -7,3 +7,16 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
 from starlette import status
+
+from src.database.db import get_db
+from src.repository import users as repository_users
+
+class Auth:
+
+    pwd_context = CryptContext(schemes='bcrypt', deprecated='auto')
+    SECRET_KEY = '7a4a0a0dd381e393a78d75bfc58a9cbc3bb2c985df98658e3aadf0aef4c4e5bb' # TODO убрать в ENV файл
+    ALGORITHM = 'HS256'
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+    def get_password(self, password:str):
+        return self.pwd_context.hash(password)
