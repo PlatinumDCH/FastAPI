c95680a25da31d12188d1b51e4f51212153c3a31
PlatinumDCH <127415898+PlatinumDCH@users.noreply.github.com>
2024-12-04 12:11:59 +0200

    Update auth.py

--- src/services/auth.py
+++ src/services/auth.py
@@ -14,7 +14,7 @@ from src.repository import users as repository_users
 class Auth:
 
     pwd_context = CryptContext(schemes='bcrypt', deprecated='auto')
-    SECRET_KEY = '7a4a0a0dd381e393a78d75bfc58a9cbc3bb2c985df98658e3aadf0aef4c4e5bb' # TODO убрать в ENV файл
+    SECRET_KEY = '7a4a0a0dd381e393a78d75bfc58a9cbc3bb2c985df98658e3aadf0aef4c4e5bb'
     ALGORITHM = 'HS256'
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
