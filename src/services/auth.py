f3194de3472f89d5d34a8323c451025c51fb83ed
dima cheban <dimacheban23@gmail.com>
2024-12-04 14:44:08 +0200

    исправил ошибку в названии пакета

--- src/services/auth.py
+++ src/services/auth.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from fastapi import Depends, HTTPException
-from pathlib.context import CryptContext
+from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
