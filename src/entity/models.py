8abec9b565533bdb9cdb63e1c9a588e7ce7216df
dima cheban <dimacheban23@gmail.com>
2024-12-06 12:04:45 +0200

    добавил роли

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,7 +1,8 @@
+import enum
 from datetime import date
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, ForeignKey, DateTime, func
+from sqlalchemy import String, Integer, ForeignKey, DateTime, func, Enum
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 
@@ -27,6 +28,11 @@ class Todo(Base):
     users_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'),nullable=True)
     user: Mapped['User'] = relationship('User', backref='todos', lazy='joined')
 
+class Role(enum.Enum):
+    admin: str = 'admin'
+    moderator: str = 'moderator'
+    user: str = 'user'
+
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -38,3 +44,4 @@ class User(Base):
     created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
     updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(),
                                              onupdate=func.now())
+    role: Mapped[Enum] = mapped_column('role', Enum(Role), default=Role.user)
